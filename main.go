7b40e90d8e2844c08b9598238a4685ff67110d1e
destroit <xn0tdev@gmail.com>
2025-04-12 09:32:30 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
+	"nn/api"
 	"os"
 	"strings"
 	"time"
-	"nn/api"
 )
 
 var globalModel *api.Model
@@ -14,47 +15,63 @@ var globalModel *api.Model
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	if len(os.Args) < 2 {
-		fmt.Println("Использование: go run main.go [команда] [аргументы]")
+		fmt.Println("Usage: ./nn [command] [arguments] [--temp=<value>]")
 		return
 	}
+
 	args := os.Args[1:]
 	for i := 0; i < len(args); {
+		if strings.HasPrefix(args[i], "--temp=") {
+			i++
+			continue
+		}
 		command := args[i]
 		switch command {
 		case "learn":
 			if i+1 >= len(args) {
-				fmt.Println("Использование: learn <filename.txt>")
+				fmt.Println("Usage: learn <filename.txt>")
 				return
 			}
 			api.Learning(args[i+1])
 			i += 2
 		case "load":
 			if i+1 >= len(args) {
-				fmt.Println("Использование: load <filename.bin>")
+				fmt.Println("Usage: load <filename.bin>")
 				return
 			}
 			var err error
 			globalModel, err = api.LoadFromFile(args[i+1])
 			if err != nil {
-				fmt.Println("Ошибка загрузки модели:", err)
+				fmt.Println("Error loading model:", err)
 				return
 			}
-			fmt.Println("Модель успешно загружена.")
+			fmt.Println("Model loaded successfully.")
 			i += 2
 		case "ask":
 			if i+1 >= len(args) {
-				fmt.Println("Использование: ask <вопрос>")
+				fmt.Println("Usage: ask <question>")
 				return
 			}
 			if globalModel == nil {
-				fmt.Println("Ошибка: модель не загружена.")
+				fmt.Println("Error: Model not loaded.")
 				return
 			}
 			question := strings.Join(args[i+1:], " ")
-			api.InputOutput(globalModel, question)
+			tempFlag := flag.Float64("temp", 1.2, "Temperature for response creativity") // Понижена температура
+			flag.CommandLine.Parse(os.Args[1:])
+			fmt.Println("Response:", api.InputOutput(globalModel, question, *tempFlag))
+			i = len(args)
+		case "chat":
+			if globalModel == nil {
+				fmt.Println("Error: Model not loaded.")
+				return
+			}
+			tempFlag := flag.Float64("temp", 1.2, "Temperature for response creativity") // Понижена температура
+			flag.CommandLine.Parse(os.Args[1:])
+			api.Chat(globalModel, *tempFlag)
 			i = len(args)
 		default:
-			fmt.Println("Неизвестная команда:", command)
+			fmt.Println("Unknown command:", command)
 			return
 		}
 	}
